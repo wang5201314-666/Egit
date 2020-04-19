ea2bbd20b0fcee796b180e4ca7d0348045721746
wang5201314-666 <185687176@qq.com>
2020-04-19 22:24:13 +0800

    创建一个新的分支--w6666

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -4,6 +4,7 @@ public class Love {
 	public static void main(String[] args) {
 		System.out.println("我喜欢你  --w666111");
 		System.out.println("我也喜欢你 --w6666");
+		System.out.println("创建一个新的分支--w6666");
 	}
 
 }
