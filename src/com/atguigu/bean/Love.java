a35600439937d6f1f28a65318e6a637841cb744a
wang5201314-666 <63486810+wang5201314-666@users.noreply.github.com>
2020-04-19 21:21:11 +0800

    Update Love.java
    github修改

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -4,7 +4,8 @@ package com.atguigu.bean;
 public class Love {
 	public static void main(String[] args) {
 		System.out.println("我喜欢你  --w666");
-		System.out.println("Love.main");
+		System.out.println("我爱你	--w666");
+		
 	}
 
 
