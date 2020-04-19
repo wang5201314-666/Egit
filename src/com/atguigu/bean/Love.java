9d0000376cffb81ca6e1bd0b5754737b00cbd6bf
wang5201314-666 <185687176@qq.com>
2020-04-19 21:46:40 +0800

    111

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -2,7 +2,8 @@ package com.atguigu.bean;
 
 public class Love {
 	public static void main(String[] args) {
-		System.out.println("我喜欢你  --w666");
+		System.out.println("我喜欢你  --w666111");
+		System.out.println("我也喜欢你 --w6666");
 	}
 
 }
